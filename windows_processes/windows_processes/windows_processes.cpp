4233e4da2dfc40a539b611cdba31fb048eaa891c
Vladislav Lezhnew <vladislav.lezhnew@gmail.com>
2025-10-18 16:27:42 +0300

    исправлен баг при работе с массивом, когда вместо 10 выводило 2573

--- windows_processes/windows_processes/windows_processes.cpp
+++ windows_processes/windows_processes/windows_processes.cpp
@@ -1,4 +1,4 @@
-﻿#include <windows.h>
+#include <windows.h>
 #include <iostream>
 #include <vector>
 
@@ -6,9 +6,14 @@ using namespace std;
 
 // Функция для работы в режиме потомка
 void ChildMode() {
+    HANDLE hStdIn = GetStdHandle(STD_INPUT_HANDLE);
+    HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    
     int size;
+    DWORD bytesRead;
+    
     // Чтение размера массива из stdin
-    if (!cin.read(reinterpret_cast<char*>(&size), sizeof(size))) {
+    if (!ReadFile(hStdIn, &size, sizeof(size), &bytesRead, NULL) || bytesRead != sizeof(size)) {
         cerr << "Child: Failed to read array size" << endl;
         return;
     }
@@ -16,25 +21,30 @@ void ChildMode() {
     vector<int> array(size);
     // Чтение элементов массива
     for (int i = 0; i < size; ++i) {
-        if (!cin.read(reinterpret_cast<char*>(&array[i]), sizeof(array[i]))) {
+        if (!ReadFile(hStdIn, &array[i], sizeof(array[i]), &bytesRead, NULL) || bytesRead != sizeof(array[i])) {
             cerr << "Child: Failed to read array element" << endl;
             return;
         }
     }
 
-    // Поиск минимального элемента
+    // Поиск максимального элемента
     if (array.empty()) {
+        // Отправляем специальное значение для пустого массива
+        int errorResult = -1;
+        DWORD bytesWritten;
+        WriteFile(hStdOut, &errorResult, sizeof(errorResult), &bytesWritten, NULL);
         cerr << "Child: Empty array" << endl;
         return;
     }
 
-    int minElement = array[0];
-    for (int i = 0; i < array.size(); ++i) {
-        if (array[i] < minElement) minElement = array[i];
+    int maxElement = array[0];
+    for (int i = 1; i < array.size(); ++i) {
+        if (array[i] > maxElement) maxElement = array[i];
     }
 
     // Запись результата в stdout
-    if (!cout.write(reinterpret_cast<char*>(&minElement), sizeof(minElement))) {
+    DWORD bytesWritten;
+    if (!WriteFile(hStdOut, &maxElement, sizeof(maxElement), &bytesWritten, NULL)) {
         cerr << "Child: Failed to write result" << endl;
     }
 }
@@ -73,7 +83,7 @@ void ParentMode() {
     GetModuleFileNameA(NULL, selfPath, MAX_PATH);
 
     // Формирование командной строки с использованием char*
-    char commandLine[MAX_PATH + 10]; // +10 для " child" и запаса
+    char commandLine[MAX_PATH + 10];
     strcpy_s(commandLine, selfPath);
     strcat_s(commandLine, " child");
 
@@ -101,6 +111,7 @@ void ParentMode() {
     // Закрываем ненужные дескрипторы в родительском процессе
     CloseHandle(hStdInRead);
     CloseHandle(hStdOutWrite);
+    CloseHandle(pi.hThread); // Закрываем handle потока сразу
 
     // Ввод данных массива
     int size;
@@ -112,7 +123,6 @@ void ParentMode() {
         CloseHandle(hStdInWrite);
         CloseHandle(hStdOutRead);
         CloseHandle(pi.hProcess);
-        CloseHandle(pi.hThread);
         return;
     }
 
@@ -136,23 +146,44 @@ void ParentMode() {
     }
 
     // Закрываем дескриптор записи в stdin дочернего процесса
+    // Это сигнализирует конец данных
     CloseHandle(hStdInWrite);
 
     // Чтение результата из дочернего процесса
     int result;
     DWORD bytesRead;
-    if (ReadFile(hStdOutRead, &result, sizeof(result), &bytesRead, NULL)) {
-        cout << "Min element: " << result << endl;
-    }
-    else {
-        cerr << "Parent: Failed to read result" << endl;
+    
+    // Ждем данные с таймаутом (5000 мс)
+    DWORD waitResult = WaitForSingleObject(pi.hProcess, 5000);
+    if (waitResult == WAIT_OBJECT_0) {
+        // Процесс завершился, пробуем прочитать если что-то осталось в буфере
+        if (ReadFile(hStdOutRead, &result, sizeof(result), &bytesRead, NULL) && bytesRead == sizeof(result)) {
+            if (result == -1) {
+                cout << "Child reported: Empty array" << endl;
+            } else {
+                cout << "Max element: " << result << endl;
+            }
+        } else {
+            // Пробуем получить код возврата
+            DWORD exitCode;
+            if (GetExitCodeProcess(pi.hProcess, &exitCode)) {
+                cerr << "Parent: Child process exited with code " << exitCode << ", but no result received" << endl;
+            } else {
+                cerr << "Parent: Failed to read result and get exit code" << endl;
+            }
+        }
+    } else if (waitResult == WAIT_TIMEOUT) {
+        // Таймаут - процесс завис
+        cerr << "Parent: Child process timeout" << endl;
+        TerminateProcess(pi.hProcess, 1);
+    } else {
+        // Ошибка ожидания
+        cerr << "Parent: Wait for child process failed" << endl;
     }
 
-    // Ожидание завершения и закрытие дескрипторов
-    WaitForSingleObject(pi.hProcess, INFINITE);
+    // Закрытие дескрипторов
     CloseHandle(hStdOutRead);
     CloseHandle(pi.hProcess);
-    CloseHandle(pi.hThread);
 }
 
 int main(int argc, char* argv[]) {
